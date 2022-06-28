a87bba21f5414132030ed79630fb3b081775a93e
Angela Robinette~ <angela@dev-angela.com>
2022-06-27 20:26:11 -0700

    Waiting for API key to initialize

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Creates an object to store functions and variables for the API
+// 1. Use apiKey to access weather from openweathermap.org
 let weather = {
-    
+    "apiKey": "6ebd44e559df99375a356aa98f5496f3"
 }
